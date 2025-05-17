f2c91f427ef2a09ddff65bab12cb9e6af9071317
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2025-05-17 14:38:49 +0530

    First Program

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Where are you from?");
+            string? location = Console.ReadLine();
+            if (location != null)
+            {
+                Console.WriteLine($"You are from {location}.");
+            }
+            else
+            {
+                Console.WriteLine("Location is not provided.");
+            }
+        }
+    }
+}
