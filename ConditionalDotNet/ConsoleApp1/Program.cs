48b88e4a38a5e65ae8f33559bec9f319cce0070c
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2025-05-31 08:58:50 +0530

    Conditionals

---
+++ ConditionalDotNet/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Conditional 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
+            if(age < 18)
+            {
+                Console.WriteLine("You are a minor.");
+            }
+            else if (age >= 18 && age < 65)
+            {
+                Console.WriteLine("You are an adult.");
+            }
+            else
+            {
+                Console.WriteLine("You are a senior citizen.");
+            }
+        }
+    }
+}
