047ec65685e8f4d324cb176e6c2eb38e80581012
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2025-06-21 11:55:48 +0530

    Basics - Tuples and branches

---
+++ TupleFunction/TupleFunction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TuplesWork;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[] { 54, 21, 21, 65, 76, 9, 3, 99 };
+
+        var minAndMax = GetMinAndMax(numbers);
+
+        Console.WriteLine(minAndMax.Item1); // Prints min value
+        Console.WriteLine(minAndMax.Item2);
+    }
+
+    static (int, int) GetMinAndMax(int[] nums)
+    {
+        int min = nums[0];
+        int max = nums[0];
+
+        for(int i=0; i < nums.Length; i++)
+        {
+            if (nums[i] < min)
+            {
+                min = nums[i];
+            }
+
+            if(nums[i] > max)
+            {
+                max = nums[i];
+            }
+        }
+
+        return (min, max);
+    }
+}
